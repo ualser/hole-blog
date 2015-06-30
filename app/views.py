73580647786cb181fb5f11513da98d585bbaed59
ubuntu <stas@ubuntu.(none)>
2015-06-30 08:09:46 +0300

    view messages for one user functionality added

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
-from flask import render_template
-from app import app
+from flask import render_template, request
+from app import app, db, models
+import datetime
 
 @app.route('/')
 @app.route('/index')
@@ -19,3 +20,26 @@ def index():
                            title='Home',
                            user=user,
                            posts=posts)
+
+@app.route('/add_user')
+def add_user():
+	u = models.User(nickname='sbres', email='sbres@email.com')
+	db.session.add(u)
+	db.session.commit()
+	return 'user added!'
+
+@app.route('/add_post')
+def add_post():
+	u = models.User.query.get(3)
+	p = models.Post(body='first post, bitch!', timestamp=datetime.datetime.utcnow(), author=u)
+	db.session.add(p)
+	db.session.commit()
+	return "post added"
+
+@app.route('/view_posts')
+def view_posts():
+	user = request.args.get('user')
+	u = models.User.query.get(user)
+	posts = u.posts.all()
+	print "\n+++++++++++++++++++++++++\n", posts, "\n++++++++++++++++++++++++++\n"
+	return "OK" 
